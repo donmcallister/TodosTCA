01fd0dfd506984b064b29bf17571d0459769371a
Don <mcallisterda@gmail.com>
2020-05-21 14:13:39 -0400

    Initial Commit

---
+++ TodosTCA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodosTCA
+//
+//  Created by Donald McAllister on 5/21/20.
+//  Copyright © 2020 Donald McAllister. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

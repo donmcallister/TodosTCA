574fc251d17e298c436364c3f5e396c31093536e
Don <mcallisterda@gmail.com>
2020-05-21 14:38:39 -0400

    core domain of application added and dummy Todos in previews

--- TodosTCA/ContentView.swift
+++ TodosTCA/ContentView.swift
@@ -7,15 +7,82 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
+struct Todo: Equatable, Identifiable {
+    var description = ""
+    let id: UUID
+    var isComplete = false
+}
+
+/**************************
+ Core Domain of Application
+ **************************/
+
+struct AppState: Equatable {
+    var todos: [Todo] = []
+}
+
+enum AppAction {
+    
+}
+
+struct AppEnvironment {}
+
+/**************************
+        APP REDUCER
+ **************************/
+//hand closure to the initializer of Reducer:
+let appReducer = Reducer<AppState, AppAction, AppEnvironment> { state, action, environment in
+    switch action {
+        //business logic
+    }
+}
+
+
 
 struct ContentView: View {
+    let store: Store<AppState, AppAction>
+    
     var body: some View {
-        Text("Hello, World!")
+        WithViewStore(self.store) { viewStore in
+            NavigationView {
+                List {
+                    ForEach(viewStore.state.todos) { todo in
+                        Text(todo.description)
+                    }
+                }
+            .navigationBarTitle("Todos")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(
+            store: Store(
+                initialState: AppState(
+                    todos: [
+                        Todo(
+                            description: "Milk",
+                            id: UUID(),
+                            isComplete: false
+                        ),
+                        Todo(
+                            description: "Eggs",
+                            id: UUID(),
+                            isComplete: false
+                        ),
+                        Todo(
+                            description: "Hand Soap",
+                            id: UUID(),
+                            isComplete: false
+                        ),
+                    ]
+                ),
+                reducer: appReducer,
+                environment: AppEnvironment())
+        )
     }
 }
